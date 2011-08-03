b3078f4c3b41945b9add90c5f8c2bb8c46ac70fb
Richard Crowley <r@rcrowley.org>
2011-08-03 15:24:13 +0000

    Require -b argument.

--- lib/gp.sh
+++ lib/gp.sh
@@ -6,7 +6,7 @@ usage() {
 
 # The default repository and branch can be overridden by the environment.
 [ -z "$GITPAID_REPO" ] && REPO="$HOME/.gitpaid" || REPO="$GITPAID_REPO"
-[ -z "$GITPAID_BRANCH" ] && BRANCH="master" || BRANCH="$GITPAID_BRANCH"
+[ -z "$GITPAID_BRANCH" ] && usage || BRANCH="$GITPAID_BRANCH"
 
 # Overload the committer's name with the name of the program used to make
 # the commit, which will allow tracking begin and end times conveniently.
